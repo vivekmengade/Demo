0c8c7724eda179bea5daa0fcef8e6b47c5c9d26a
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2023-09-15 11:35:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) { entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+  const hiddenElements = document.querySelectorAll('.main'); hiddenElements.forEach((el) => observer.observe (el));
+  
